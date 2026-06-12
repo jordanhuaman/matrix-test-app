5c0598199aa0c9e52b67f9a881463c2d6345ffd6
jordanhuaman <jordanelcracken@gmail.com>
2026-06-11 22:05:21 -0500

    Feat: implement user registration and validation logic

--- go-api/main.go
+++ go-api/main.go
@@ -3,20 +3,39 @@ package main
 import (
 	"log"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v3"
 	"github.com/joho/godotenv"
+	"github.com/jordanhuaman/go-api/src/framework"
 	database "github.com/jordanhuaman/go-api/src/framework"
 )
+
+// 1. Creamos una estructura propia que envuelve al validador original
+type GoPlaygroundValidator struct {
+	Validator *validator.Validate
+}
+
+// 2. Implementamos el método Validate que exige la interfaz fiber.StructValidator de v3
+func (v *GoPlaygroundValidator) Validate(out any) error {
+	return v.Validator.Struct(out)
+}
+
+func welcome(c fiber.Ctx) error {
+	return c.SendString("Welcome to my API")
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api", welcome)
+	app.Post("/user/register", framework.CreateUser)
+}
 
 func main() {
 	godotenv.Load()
 	database.ConnectDb()
-	// Create new Fiber instance
-	app := fiber.New()
-	// Create new GET route on path "/"
-	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-	// Start server on http://localhost:3000
+
+	cv := &GoPlaygroundValidator{Validator: validator.New()}
+
+	app := fiber.New(fiber.Config{StructValidator: cv})
+	setupRoutes(app)
 	log.Fatal(app.Listen(":3000"))
 }
--- go-api/src/framework/db.config.go
+++ go-api/src/framework/db.config.go
@@ -1,4 +1,4 @@
-package database
+package framework
 
 import (
 	"fmt"
